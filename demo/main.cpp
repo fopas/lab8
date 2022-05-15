bbb4e277bd24cd242fe9c0fdbb10fa5c33aa80b9
Artem Sharov <ash.03@yandex.ru>
2022-05-15 04:38:52 -0700

    final

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,12 @@
-#include <example.hpp>
+// Copyright 2022 Your Name <your_email>
 
+#include "client.hpp"
 int main() {
-  example();
+  Client cl;
+  // {"input":"hel"}
+  cl.request();
+  std::cout << std::endl;
+  std::cout << "RESPONSE: " << std::endl;
+  std::cout << std::endl;
+  cl.start();
 }
